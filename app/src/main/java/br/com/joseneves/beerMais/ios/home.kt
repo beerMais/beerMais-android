ad1cd7cb84718262989583eb67dc132d9ba9c04a
Jose Neves <contato@joseneves.com.br>
2019-01-27 05:49:53 -0200

    Align beer cells to center

--- app/src/main/java/br/com/joseneves/beerMais/ios/home.kt
+++ app/src/main/java/br/com/joseneves/beerMais/ios/home.kt
@@ -1,23 +1,24 @@
 package br.com.joseneves.beerMais.ios
 
+import android.graphics.Rect
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.app_bar_home.*
 
 class home : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    private var mRecyclerView: RecyclerView? = null
+    private var beerRecyclerView: RecyclerView? = null
     private var mAdapter: RecyclerView.Adapter<*>? = null
-    var beers: ArrayList<Beer> = ArrayList()
+    private var beers: ArrayList<Beer> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,19 +39,19 @@ class home : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListene
         nav_view.setNavigationItemSelectedListener(this)
 
         for (i in 0..4) {
-            val user = Beer()
-            user.id = i
-            user.amount = i
-            user.brand = "Brand $i"
-            user.amount = i
+            val beer = Beer()
+            beer.id = i
+            beer.amount = i
+            beer.brand = "Brand $i"
+            beer.amount = i
 
-            beers.add(user)
+            beers.add(beer)
         }
-        mRecyclerView = findViewById(R.id.my_recycler_view)
-        val mLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        mRecyclerView!!.layoutManager = mLayoutManager
+        beerRecyclerView = findViewById(R.id.beer_recyclerView)
+        beerRecyclerView!!.layoutManager = GridLayoutManager(this, 2)
+        beerRecyclerView!!.addItemDecoration(RecyclerViewMargin())
         mAdapter = BeerAdapter(beers)
-        mRecyclerView!!.adapter = mAdapter
+        beerRecyclerView!!.adapter = mAdapter
     }
 
     override fun onBackPressed() {
@@ -90,5 +91,19 @@ class home : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListene
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    inner class RecyclerViewMargin: RecyclerView.ItemDecoration() {
+        override fun getItemOffsets(
+            outRect: Rect, view: View,
+            parent: RecyclerView, state: RecyclerView.State
+        ) {
+            val totalWidth = parent.width
+            val cardWidth = 150
+
+            var sidePadding = (totalWidth - cardWidth) / 14
+            sidePadding = Math.max(0, sidePadding)
+            outRect.set(sidePadding, 0, 0, 0)
+        }
     }
 }
