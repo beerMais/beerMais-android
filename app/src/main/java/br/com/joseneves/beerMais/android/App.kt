d99a84f811eb45a967c8f11c3eb74ed54b3a1414
Jose Neves <contato@joseneves.com.br>
2023-08-19 13:10:38 -0300

    upgrade dependencies

--- app/src/main/java/br/com/joseneves/beerMais/android/App.kt
+++ app/src/main/java/br/com/joseneves/beerMais/android/App.kt
@@ -10,7 +10,7 @@ class App: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713")
+        MobileAds.initialize(this) {}
         firebaseAnalytics = FirebaseAnalytics.getInstance(this)
     }
 }
